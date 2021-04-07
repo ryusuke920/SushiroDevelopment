93b2ed835da8e24f2b8fab7657895409442b320b
ryusuke920 <ryusuke920@AtCoder-Library.com>
2021-04-07 17:03:31 +0900

    I can get the sushi information

--- sushiro.py
+++ sushiro.py
@@ -1,3 +1,10 @@
+"""
+/Q&A/
+・スシローのスクレイピングを始めた理由 ... 寿司が好きだから。
+・スシローの品数 ... 166品（2021/04/07現在）
+・役に立つの？ ... 役に立ちません。趣味です。
+"""
+
 import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
@@ -10,7 +17,8 @@ soup = BeautifulSoup(html, "lxml") # BeautifulSoupオブジェクトに変更
 # 寿司の名前を取得する関数
 def get_sushi_name():
     sushi_name = soup.select('li > a > span.txt-wrap > span.ttl') # 寿司の名前のspanタグを抽出
-    sushi_name_list = [sushi.text for sushi in sushi_name] # spanタグ内の名前のみを抽出
+    sushi_name_list = [sushi.text for sushi in sushi_name] # spanタグ内の名称のみを抽出
+
     return sushi_name_list
 
 # 寿司のpng形式のURLを取得する関数
@@ -20,11 +28,12 @@ def get_sushi_image_url():
     for img in img_link_list:
         img_url = (img.attrs["src"]) # < > や img タグを取り除き、URLのみを抽出
         img_list.append(img_url)
+
     return img_list[6:] # 最初の6枚はジャンル別画像なのでスキップ
 
-# 寿司のpngを名前指定で取り込む関数
+# 寿司の.pngを名称指定で取り込む関数
 def get_sushi_image():
-    sushi_name = get_sushi_name() # 寿司の名前をリストで取得
+    sushi_name = get_sushi_name() # 寿司の名称をリストで取得
     for number, image_url in enumerate(get_sushi_image_url()):
         print(F"{number + 1}皿目、{sushi_name[number]}の取り込みが完了しました。") # debug用
         img_save_path = Path(F"/Users/ryusuke/documents/git/SushiroDevelopment/img/{sushi_name[number]}.png")
@@ -33,5 +42,42 @@ def get_sushi_image():
         with open(img_save_path, "wb") as product:
             product.write(image)
 
+# 寿司の情報（値段（税別）・カロリー）を取得する関数
+def get_sushi_price_calorie():
 
-get_sushi_image()
+    # 値段について
+    price_list = [] # 値段（税別）を格納する配列
+    price_calorie_class_information = soup.select("li > a > span.txt-wrap > span.price")
+    price_calorie_string_information = [list(information.text.split()) for information in price_calorie_class_information] # ex.) ['280円（税込308円）', '293kcal']
+    for price_string_information in price_calorie_string_information:
+        price_list.append(int(price_string_information[0].split("円")[0]))
+    
+    # カロリーについて
+    calorie_list = [] # カロリーを格納する配列
+    for calorie_string_information in price_calorie_string_information:
+        if len(calorie_string_information) == 2: # 砂糖なしの場合
+            calorie_list.append(int(calorie_string_information[1].split("k")[0]))
+        else: # 砂糖ありの場合 <- length == 3
+            calorie_list.append(int(calorie_string_information[2].split("k")[0]))   
+
+    return price_list, calorie_list
+
+def get_sushi_information(): # 寿司の情報（名称・値段・カロリー）をまとめて返す関数
+    name_information = get_sushi_name() # 寿司の名称を取得
+    price_information = get_sushi_price_calorie()[0] # 寿司の値段を取得
+    calorie_information = get_sushi_price_calorie()[1] # 寿司のカロリーを取得
+    sushi_information = []
+    for number in range(len(name_information)):
+        sushi_information.append([name_information[number], price_information[number], calorie_information[number]])
+
+    return sushi_information # 名称・値段（税別）・カロリー
+
+
+"""
+〜画像の取り込み方〜
+⓵ 取り込みたい階層にターミナルで cd を使って移動します
+② 実行します。終わりです。
+"""
+# get_sushi_image() # 寿司の画像が欲しい時に実行
+
+print(*get_sushi_information(),sep="\n")
